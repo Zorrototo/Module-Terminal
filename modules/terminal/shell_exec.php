9504b72772f86259e65d050cf836ceacc611f588
®omano <zebilaweed@free.fr>
2017-08-24 23:57:11 +0200

    Update shell_exec.php

--- modules/terminal/shell_exec.php
+++ modules/terminal/shell_exec.php
@@ -1,26 +1,24 @@
 <?php
 function exec_ogp_module() {
 
-	$command = $_POST["command"]; 
-	$output = shell_exec("$command  2>&1");
+	$command = $_POST["command"];
 	echo "<div class='shell-wrap'>
 		<p class='shell-top-bar'>Terminal</p>
 		<ul class='shell-body'>";
-		if ( isset($command) && !empty($command)){
-			echo "<li>&nbsp;</li>
+	if ( isset($command) && !empty($command)){
+		$output = shell_exec("$command  2>&1");
+		echo "<li>&nbsp;</li>
 			<li>$command</li>
 			<li>&nbsp;</li>";
-		}
-		foreach(preg_split("/((\r?\n)|(\r\n?))/", $output) as $line){
-			echo "<li>$line</li>";
-		}
-		echo "<li>&nbsp;</li>
-			<li><form action='home.php?m=terminal&p=terminal' method='post'>
-			<input type='text' name='command' autofocus='autofocus'/>
-			<input type='submit' style='height: 0px; width: 0px; border: none; padding: 0px; margin: 0px;' hidefocus='true' />
-			</form></li>"; ?>
+	}
+	foreach(preg_split("/((\r?\n)|(\r\n?))/", $output) as $line){
+		echo "<li>$line</li>";
+	}
+	echo "<li>&nbsp;</li>
+		<li><form action='home.php?m=terminal&p=terminal' method='post'>
+		<input type='text' name='command' autofocus='autofocus'/>
+		<input type='submit' style='height: 0px; width: 0px; border: none; padding: 0px; margin: 0px;' hidefocus='true' /></form></li>
 		</ul>
-	</div>
-<?php 
-} 
+		</div>";
+}
 ?>
